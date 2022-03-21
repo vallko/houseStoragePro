475914d07afcff7a1883bf242345116549a3d1e0
ValeriDimitrov <61822548+vallko@users.noreply.github.com>
2022-03-21 23:05:39 +0200

    table model

--- mySoft.java
+++ mySoft.java
@@ -2,7 +2,8 @@ import javax.swing.*;
 
 public class mySoft {
     public static void main(String[] args) {
-        JFrame frame = new MSPgui("My storageHouse Pro");
+        JFrame frame = new MSPgui("houseStorage Pro");
         frame.setVisible(true);
+
     }
 }
---
+++ table.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class table extends JTable {
+
+    void DefaultTableModel(int rowCount, int columnCount) {
+
+    }
+    void addColumn(Object columnName, Object[] columnData){
+
+    }
+    void	addRow(Object[] rowData){
+
+    }
+
+    public static void main(String[] args) {
+
+
+        String[] columnNames = {"Name", "Package", "Quantity", "Price"};
+
+        JTable table1 = new JTable();
+
+    }
+
+}
